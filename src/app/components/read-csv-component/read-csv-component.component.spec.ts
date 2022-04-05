f10db92d6c240432fc023117ec9d991a072074b2
DallinLemon <dallinlemon@gmail.com>
2022-04-05 16:33:29 -0600

    feat(main): worked on csv file reader

---
+++ src/app/components/read-csv-component/read-csv-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReadCsvComponentComponent } from './read-csv-component.component';
+
+describe('ReadCsvComponentComponent', () => {
+  let component: ReadCsvComponentComponent;
+  let fixture: ComponentFixture<ReadCsvComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReadCsvComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReadCsvComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
