775af9db46cd95e42b11139d1a2c41728d0a327b
DallinLemon <dallinlemon@gmail.com>
2022-05-21 15:37:08 -0600

    moved shared folder back

--- angular-frontend/src/app/components/side-bar-component/side-bar.component.ts
+++ angular-frontend/src/app/components/side-bar-component/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar-component',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(
+  ): void {
+
   }
 
+  public goToHome(): void {
+    this.router.navigate(['/home']);
+  }
+
+  public goToBudget(): void {
+    this.router.navigate(['/budget']);
+  }
 }
