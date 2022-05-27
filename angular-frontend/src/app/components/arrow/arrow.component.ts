b8911a8ab547e762278ba43b73909fb69278cfdb
DallinLemon <dallinlemon@gmail.com>
2022-05-27 07:35:46 -0600

    chore refactored to make arrows point down by default

--- angular-frontend/src/app/components/arrow/arrow.component.ts
+++ angular-frontend/src/app/components/arrow/arrow.component.ts
@@ -9,8 +9,8 @@ import Group from 'shared/models/group.model';
   animations: [
     // Each unique animation requires its own trigger. The first argument of the trigger function is the name
     trigger('rotatedState', [
-        state('default', style({ transform: 'rotate(0)' })),
-        state('rotated', style({ transform: 'rotate(90deg)' })),
+        state('rotated', style({ transform: 'rotate(0)' })),
+        state('default', style({ transform: 'rotate(90deg)' })),
         transition('rotated => default', animate('400ms ease-out')),
         transition('default => rotated', animate('400ms ease-in'))
     ])
@@ -25,7 +25,7 @@ export class Arrow implements OnInit {
 
   ngOnInit(): void {
   }
-  
+
   rotate() {
       this.state = (this.state === 'default' ? 'rotated' : 'default');
   }
